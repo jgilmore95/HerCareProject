097a1d521e83948edf08786281486dbbdf2990ea
Mahmood Subhi <eng740@yahoo.com>
2018-12-01 22:14:50 -0500

    M1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function updateForm(volunteerName) {
+    document.getElementById("volunteer_display").innerHTML = "Volunteer Profie goes here . . . "
+}
+
+function confirmDeleteProfile() {
+    var r = confirm("Are you sure you want to Delete Volunteer Profile?");
+    if (r == true) {
+        alert ("Volunteer Profile is been deleted!");
+        window.location.replace("./VolunteerManagement");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+function confirmCancelEditVolunteerProfile() {
+    var r = confirm("Are you sure you want to Cancel? all changes will be discarded?");
+    if (r == true) {
+        window.location.replace("./VolunteerManagement");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function confirmCancelSaveVolunteerProfile() {
+    var r = confirm("Are you sure you want to Save Changes?");
+    if (r == true) {
+        alert ("Changes Volunteer Profile saved!");
+        window.location.replace("./VolunteerManagement");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
