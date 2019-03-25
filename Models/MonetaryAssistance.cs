f7c49d13d1ab2d7762b1fe2731702f780746d5a5
CJohnson96 <41927005+CJohnson96@users.noreply.github.com>
2019-03-25 13:49:52 -0400

    Monetary Assistance first changes
    will be testing on another device

--- Models/MonetaryAssistance.cs
+++ Models/MonetaryAssistance.cs
@@ -9,46 +9,15 @@ namespace her_care.Models{
 
 public class MonetaryAssistance
 {
-    [Required(ErrorMessage = "Please enter the client's first name")]
-     public string FirstNameMA {get; set;}
-
-    [Required(ErrorMessage = "Please enter client's last name")]
-    public string LastNameMA {get; set;}
     
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-    public DateTime MonetaryDate { get; set; }
-
-    [Required]
-    [RegularExpression("[^0-9]*$", ErrorMessage = "Please enter valid street address")]
-    public string streetNumberMA { get; set; }
-
-    [Required(ErrorMessage = "Please enter street's name")]
-     public string StreetNameMA {get; set;}
-
-     [Required]
-    [RegularExpression("[^0-9]*$", ErrorMessage = "Please enter valid street address")]
-    public string ZipcodeMA { get; set; }
-
-    [Required(ErrorMessage = "Please enter state's name")]
-     public string StateMA {get; set;}
-
-     [Required (ErrorMessage="Required")]
-     [RegularExpression(@"^(\d{10})$", ErrorMessage = "Please enter a phone number")]
-    public string PhoneMA { get; set; }
-
-    [Required(ErrorMessage = "Must provide an e-mail address")]   
-    [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
-    public string EmailMA { get; set; }
-
-    [Required(ErrorMessage = "Please enter amount provided $000.00")]
-    public decimal MoneyMA {get; set;}
-
-    [Required(ErrorMessage = "Please enter the client's Full Name")]
-    public string ClientSigMA {get; set;}
-
+    [Required(ErrorMessage = "Please Select A Service")]
+    public int Services {get; set;}
+    
     [Required(ErrorMessage = "Please enter the client's Full Name")]
     public string StaffSigMA {get; set;}
+
+    [Required(ErrorMessage = "Please enter the amount provided")]
+    public decimal MoneyMA {get; set;}
 
 }
 
